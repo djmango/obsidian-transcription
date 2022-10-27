e83849b25c1f50121622404fefb1357672d5cd1c
Sulaiman Ghori <sulaiman.ghori@outlook.com>
2022-10-27 15:36:18 +0200

    Built command to get all files and validate they have the correct file extension for transcription

--- main.ts
+++ main.ts
@@ -1,13 +1,11 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Editor, MarkdownView, Plugin, PluginSettingTab, Setting, TAbstractFile, Vault } from 'obsidian';
 
 interface ObsidianTranscriptionSettings {
-	mySetting: string;
+	transcribeFileExtensions: string;
 }
 
 const DEFAULT_SETTINGS: ObsidianTranscriptionSettings = {
-	mySetting: 'default'
+	transcribeFileExtensions: 'mp3,wav,webm'
 }
 
 export default class ObsidianTranscription extends Plugin {
@@ -17,66 +15,46 @@ export default class ObsidianTranscription extends Plugin {
 		console.log('Loading Obsidian Transcription');
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'obsidian-transcription-transcribe-all-in-view',
+			name: 'Transcribe all audio files in view',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+				// Get the current filepath
+				const markdownFilePath = view.file.path;
+				console.log('Transcribing all audio files in ' + markdownFilePath);
+				
+				// Get all linked files in the markdown file
+				const filesLinked = Object.keys(view.app.metadataCache.resolvedLinks[markdownFilePath]);
+
+				// Now that we have all the files linked in the markdown file, we need to filter them by the file extensions we want to transcribe
+				const filesToTranscribe: TAbstractFile[] = [];
+				for (const linkedFilePath of filesLinked) {
+					const linkedFileExtension = linkedFilePath.split('.').pop();
+					if (linkedFileExtension === undefined || !this.settings.transcribeFileExtensions.split(',').includes(linkedFileExtension)) {
+						console.log('Skipping ' + linkedFilePath + ' because the file extension is not in the list of transcribeable file extensions');
+						continue;
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					// We now know that the file extension is in the list of transcribeable file extensions
+					const linkedFile = this.app.vault.getAbstractFileByPath(linkedFilePath);
+
+					// If the file is not found, we skip it
+					if (linkedFile === null) {
+						console.log('Could not find file ' + linkedFilePath);
+						continue;
+					}
+					filesToTranscribe.push(linkedFile)
+				}
+				
+				// Now that we have all the files to transcribe, we can transcribe them
+				for (const fileToTranscribe of filesToTranscribe) {
+					console.log('Transcribing ' + fileToTranscribe.path);
 				}
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new ObsidianTranscriptionSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -92,23 +70,7 @@ export default class ObsidianTranscription extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class ObsidianTranscriptionSettingTab extends PluginSettingTab {
 	plugin: ObsidianTranscription;
 
 	constructor(app: App, plugin: ObsidianTranscription) {
@@ -117,21 +79,21 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Settings for Obsidian Transcription' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Allowed file extensions')
+			.setDesc('Comma-separated list of file extensions to transcribe')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('mp3,wav,webm')
+				.setValue(this.plugin.settings.transcribeFileExtensions)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					console.log('Allowed file extensions: ' + value);
+					this.plugin.settings.transcribeFileExtensions = value;
 					await this.plugin.saveSettings();
 				}));
 	}
